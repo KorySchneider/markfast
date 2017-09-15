b38210b95a29d41c66ab30e1ce81632aa232c10c
Kory Schneider <kory.schneider@gmail.com>
2017-09-15 09:31:42 -0700

    Switching computers

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const express = require('express'),
-      md      = require('markdown').markdown,
-      PORT    = 3000,
-      log     = console.log,
-      app     = express();
+const express    = require('express'),
+      fileUpload = require('express-fileupload'),
+      markdown   = require('markdown').markdown,
+      PORT       = 3000,
+      log        = console.log,
+      app        = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('GET root');
+});
+
+app.post('/create', (req, res) => {
+  res.send(req.files['md-file'].data.toString());
+});
+
+app.get('/render/:name', (req, res) => {
+  res.send('render: ' + req.params.name);
 });
 
 app.listen(PORT, () => {
